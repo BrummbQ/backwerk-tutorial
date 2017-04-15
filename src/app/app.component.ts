746d26282d3f6a92ee138707877ca91d52ebf47a
Gregor Tätzner <gregor@freenet.de>
2017-04-15 21:34:42 +0200

    * connect users api and display users * fix tests for AppComponent

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,22 @@
 import { Component } from '@angular/core';
+import { DataService } from './data.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
+  providers: [DataService]
 })
 export class AppComponent {
-  title = 'app works!';
+  users = [];
+  error = "";
+
+  constructor (private dataService: DataService) {}
+
+  ngOnInit() {
+    this.dataService.getUsers()
+      .subscribe(
+				users => this.users = users,
+				error => this.error = error);
+  }
 }
---
+++ src/app/data.service.ts
@@ -0,0 +1,39 @@
+import { Injectable } from '@angular/core';
+import { Http, Response } from '@angular/http';
+
+import { Observable } from 'rxjs/Observable';
+import 'rxjs/add/operator/catch';
+import 'rxjs/add/operator/map';
+
+@Injectable()
+export class DataService {
+    baseUrl = "http://jsonplaceholder.typicode.com";
+
+    constructor (private http: Http) {}
+
+    getUsers () {
+        return this.http
+            .get(`${this.baseUrl}/users`)
+            .map(this.extractData)
+            .catch(this.handleError);
+    }
+
+    private extractData(response) {
+        let body = response.json();
+        return body || { };
+    }
+
+    private handleError (error: Response | any) {
+        // In a real world app, you might use a remote logging infrastructure
+        let errMsg: string;
+        if (error instanceof Response) {
+        const body = error.json() || '';
+        const err = body.error || JSON.stringify(body);
+        errMsg = `${error.status} - ${error.statusText || ''} ${err}`;
+        } else {
+        errMsg = error.message ? error.message : error.toString();
+        }
+        console.error(errMsg);
+        return Observable.throw(errMsg);
+    }
+}
